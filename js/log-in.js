9f5a5c19feca4f07c578c1577f6bc3b00c903856
AlejandroMigliaccio <102888662+AlejandroMigliaccio@users.noreply.github.com>
2022-07-26 21:12:34 -0300

    update

--- js/log-in.js
+++ js/log-in.js
@@ -1,6 +1,6 @@
 const resumen = [];
 
-const total = (a)=>{
+const total = (a) => {
     let reCart
     if (reCart = JSON.parse(localStorage.getItem("cart"))) {
         reCart.forEach(i => { a.push(i) });
@@ -10,36 +10,53 @@ const total = (a)=>{
 const mailIn = document.getElementById("exampleInputEmail1");
 const nameIn = document.getElementById("name");
 
-const totFinal =()=>{
+const totFinal = () => {
     const show = document.getElementById("total")
     let a = 0
     resumen.forEach(i => {
-        a+= i.price;
+        a += i.price;
     })
     return show.innerHTML += `TOTAL: $${a}`;
 }
-const confirmPer = ()=>{
+const confirmPer = () => {
     Swal.fire({
         icon: 'success',
         title: 'Operacion con exito',
         text: 'gracias por confiar',
-        footer: '<a href="">Why do I have this issue?</a>'
-      })
-    }
+    })
+}
 
-const error = () =>{
+const error = () => {
     Swal.fire({
         icon: 'error',
         title: 'Oops...',
         text: 'Asegurese de tener elementos en el carrito',
-      })
+    })
 }
 
 const pagar = document.getElementById("pagar");
-pagar.addEventListener('click', () =>{
-    resumen==0? error() : confirmPer();
-    console.log(nameIn.val())
+pagar.addEventListener('click', (event) => {
+    event.preventDefault();
+    resumen == 0 ? error() : confirmPer();
 })
+
 total(resumen);
 totFinal();
+
+const validation = { mail: false, name: false };
+
+const even = (e) => !e;
+
+const checkValidity = () => {
+    pagar.disabled = Object.values(validation).some(even);
+}
+
+mailIn.onchange = () => {
+    validation.mail = mailIn.checkValidity();
+    checkValidity();
+}
+nameIn.onchange = () => {
+    validation.name = nameIn.checkValidity();
+    checkValidity();
+}
 
